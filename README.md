dbb42d0928b4940159c8769a1a0a9ebff3e9cb4f
ss0g <55930267+ss0g@users.noreply.github.com>
2024-01-08 00:12:10 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2024-Crescendo
+
+Spartronics 4915's code for FRC's 2024 season.
---
+++ src/main/java/com/spartronics4915/frc2024/Constants.java
@@ -0,0 +1,12 @@
+package com.spartronics4915.frc2024;
+
+public final class Constants {
+    public static final class Drive {
+        public static final int kPigeon2ID = 2;
+    }
+
+    public static final class Intake {
+        public static final int kMotorID = 15;
+        public static final boolean kMotorIsInverted = false; // subject to change
+    }
+}
