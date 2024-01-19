de54566991376bcbbd5baed85d77e4828cf3f664
Troy Edwards <tntjedi.2006@gmail.com>
2024-01-18 19:47:14 -0800

    remove unused imports from Intake

--- src/main/java/com/spartronics4915/frc2024/subsystems/Intake.java
+++ src/main/java/com/spartronics4915/frc2024/subsystems/Intake.java
@@ -2,10 +2,6 @@ package com.spartronics4915.frc2024.subsystems;
 
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
