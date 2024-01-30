483326dbd3aea99e1f720016a563eadb6cca1831
Zoomer234 <fruitbat234@gmail.com>
2024-01-29 17:05:09 -0800

    forgot to add shooter the first time

---
+++ src/main/java/com/spartronics4915/frc2024/subsystems/Shooter.java
@@ -0,0 +1,10 @@
+package com.spartronics4915.frc2024.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    
+    private CANSparkMax mShooterMotor;
+
+}
