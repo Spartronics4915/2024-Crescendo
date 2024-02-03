8ed842fcbb1a35a87244f5497c9af066836bb864
Troy Edwards <tntjedi.2006@gmail.com>
2024-02-02 23:37:39 -0800

    change driver controller behavior and add documentation

--- src/main/java/com/spartronics4915/frc2024/subsystems/swerve/SwerveDrive.java
+++ src/main/java/com/spartronics4915/frc2024/subsystems/swerve/SwerveDrive.java
@@ -80,6 +80,9 @@ public class SwerveDrive extends SubsystemBase {
         setDefaultCommand(teleopDriveCommand());
     }
 
+    /**
+     * Returns the current instance of {@link SwerveDrive}.
+     */
     public static SwerveDrive getInstance() {
         if (mInstance == null) {
             mInstance = new SwerveDrive();
@@ -87,11 +90,15 @@ public class SwerveDrive extends SubsystemBase {
         return mInstance;
     }
 
+    /**
+     * Drives the robot given a {@link ChassisSpeeds} and whether to drive field relative or not.
+     */
     public void drive(final ChassisSpeeds speeds, final boolean fieldRelative) {
         drive(speeds, fieldRelative, mRotationIsIndependent);
     }
 
-    // FIXME: field relative drives faster than robot relative
+    // FIXME: field relative drives faster than robot relative (??)
+    // FIXME: need to make field relative based on pose and mirror based on alliance
     private void drive(final ChassisSpeeds speeds, final boolean fieldRelative, final boolean rotationIndependent) {
         final ChassisSpeeds _speeds;
         if (fieldRelative) {
@@ -125,9 +132,9 @@ public class SwerveDrive extends SubsystemBase {
                 final var dc = RobotContainer.getDriverController();
                 ChassisSpeeds cs = new ChassisSpeeds();
 
-                final double inputxraw = dc.getLeftY();
+                final double inputxraw = dc.getLeftY() * -1.0;
                 final double inputyraw = dc.getLeftX() * -1.0;
-                final double inputomegaraw = dc.getRightX(); // consider changing from angular velocity control to direct angle control
+                final double inputomegaraw = dc.getRightX() * -1.0; // consider changing from angular velocity control to direct angle control
 
                 final double inputx = applyResponseCurve(applyDeadband(inputxraw, kStickDeadband));
                 final double inputy = applyResponseCurve(applyDeadband(inputyraw, kStickDeadband));
@@ -150,43 +157,73 @@ public class SwerveDrive extends SubsystemBase {
         };
     }
 
+    /**
+     * Toggles whether the robot will drive field relative by default.
+     */
     public void toggleFieldRelative() {
         mIsFieldRelative = !mIsFieldRelative;
     }
 
+    /**
+     * Toggles whether the robot will drive field relative by default.
+     */
     public Command toggleFieldRelativeCommand() {
         return runOnce(this::toggleFieldRelative);
     }
 
+    /**
+     * Sets whether the robot will drive field relative by default.
+     */
     public void setFieldRelative(boolean fieldRelative) {
         mIsFieldRelative = fieldRelative;
     }
 
+    /**
+     * Returns whether the robot will drive field relative by default.
+     */
     public boolean isFieldRelative() {
         return mIsFieldRelative;
     }
 
+    /**
+     * Returns whether the rotation is decoupled.
+     */
     public boolean rotationIsDecoupled() {
         return mRotationIsIndependent;
     }
 
+    /**
+     * Decouples the rotation control from any drive commands.
+     */
     public void decoupleRotation() {
         setDesiredAngle(getAngle());
         mRotationIsIndependent = true;
     }
 
+    /**
+     * Decouples the rotation control from any drive commands.
+     */
     public Command decoupleRotationCommand() {
         return runOnce(this::decoupleRotation);
     }
 
+    /**
+     * Recouples the rotation control to any drive commands.
+     */
     public void recoupleRotation() {
         mRotationIsIndependent = false;
     }
 
+    /**
+     * Recouples the rotation control to any drive commands.
+     */
     public Command recoupleRotationCommand() {
         return runOnce(this::recoupleRotation);
     }
 
+    /**
+     * Sets the desired heading angle of the swerve drive. Only works if rotation is decoupled.
+     */
     public void setDesiredAngle(final Rotation2d angle) {
         mDesiredAngle = angle;
     }
@@ -195,28 +232,46 @@ public class SwerveDrive extends SubsystemBase {
         return (SwerveModulePosition[]) Stream.of(mModules).map((m) -> m.getPosition()).toArray(SwerveModulePosition[]::new);
     }
 
+    /**
+     * Returns the desired {@link SwerveModuleState}s of the swerve modules.
+     */
     public SwerveModuleState[] getModuleDesiredStates() {
         return (SwerveModuleState[]) Stream.of(mModules).map((m) -> m.getDesiredState()).toArray(SwerveModuleState[]::new);
     }
 
+    /**
+     * Gets the angle from the IMU.
+     */
     public Rotation2d getAngle() {
         return Rotation2d.fromDegrees(mIMU.getAngle() * -1.0);
     }
 
+    /**
+     * Adds a vision measurement to the pose estimator.
+     */
     public void addVisionMeasurement(final VisionMeasurement msmt) {
         final var p = msmt.pose().toPose2d();
         final var t = msmt.timestamp();
         addVisionMeasurement(p, t);
     }
 
+    /**
+     * Adds a vision measurement to the pose estimator.
+     */
     public void addVisionMeasurement(final Pose2d cameraPose, final double t) {
         mPoseEstimator.addVisionMeasurement(cameraPose, t);
     }
 
+    /**
+     * Gets the pose estimator's estimated pose.
+     */
     public Pose2d getPose() {
         return mPoseEstimator.getEstimatedPosition();
     }
 
+    /**
+     * Resets the pose estimator to the specified position.
+     */
     public void resetPose(final Pose2d newPose) {
         mPoseEstimator.resetPosition(getAngle(), getModulePositions(), newPose);
     }
