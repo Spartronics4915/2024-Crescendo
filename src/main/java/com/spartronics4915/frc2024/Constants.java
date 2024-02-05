a7cdeb4feca2ab711e1510aaec5f636a1df175ff
otter502 <115671877+otter502@users.noreply.github.com>
2024-02-05 10:15:08 -0800

    added autoaim

--- src/main/java/com/spartronics4915/frc2024/Constants.java
+++ src/main/java/com/spartronics4915/frc2024/Constants.java
@@ -226,8 +226,8 @@ public final class Constants {
     }
 
     public static final class AutoAimConstants {
-        public static final double kShooterSpeed = 1.0; //FIXME temp
-        public static final double kShooterHeight = 10;
+        public static final double kShooterSpeed = 3.0; //needs to be in m/s
+        public static final double kShooterHeight = 0.0;
         public static final double kMaxDistance = 10.0; //Needs units, the maximum relative distance a target can be from the robot for autoaim 
     }
 }
--- src/main/java/com/spartronics4915/frc2024/util/AutoAimFunctions.java
+++ src/main/java/com/spartronics4915/frc2024/util/AutoAimFunctions.java
@@ -2,16 +2,76 @@ package com.spartronics4915.frc2024.util;
 
 import java.util.Optional;
 
-import com.spartronics4915.frc2024.Constants.AutoAimConstants;
+import static com.spartronics4915.frc2024.Constants.AutoAimConstants.*;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import java.util.function.*;
 public class AutoAimFunctions {
 
     private static double pow(double a){return Math.pow(a, 2);}
     private static boolean check(double a) {return Double.isFinite(a) && a > 0;}
+
+    /**
+     * @param robotPose robots position on the field
+     * @param velocity the robots velocity on the field
+     * @param Target the targets position on the field
+     * @return Translation3d representing target position at collision
+     * 
+     * based on this equation: 
+     * https://www.wolframalpha.com/input?i2d=true&i=solve+Power%5B%7C%7C%7BSubscript%5Bp%2Cx%5D%2CSubscript%5Bp%2Cy%5D%2CSubscript%5Bp%2Cz%5D%7D%2B%7BSubscript%5Bu%2Cx%5D%2CSubscript%5Bu%2Cy%5D%2CSubscript%5Bu%2Cz%5D%7D*Subscript%5Bv%2Cs%5D*Subscript%5Bt%2Cc%5D%7C%7C%2C2%5D%3DPower%5B%5C%2840%29Subscript%5Bv%2Cn%5D*Subscript%5Bt%2Cc%5D%5C%2841%29%2C2%5D++for+Subscript%5Bt%2Cc%5D
+     */
+    public static Optional<Translation3d> movingAutoAim2(Pose2d robotPose, ChassisSpeeds velocity, Translation2d targetPos, double targetZ){
+        var p = targetPos.minus(robotPose.getTranslation()); //position of target relative to robot
+        var pz = targetZ - kShooterHeight;
+
+        var TempVelocity = new Translation2d(velocity.vxMetersPerSecond, velocity.vyMetersPerSecond);
+        var u = TempVelocity.div(TempVelocity.getNorm());//.times(-1.0); //reversing the direction of the unit velocity vector //no z component
+        var vs = TempVelocity.getNorm(); //speaker velocity
+        var vn = kShooterSpeed;
+        var uz = 0;
+
+        if (vs == 0.0) {
+            return Optional.of(new Translation3d(targetPos.getX(), targetPos.getY(), targetZ));
+        }
+        
+
+        var sqrtPart = Math.sqrt(
+            pow(vs) * pow(-2 * p.getX() * u.getX() - 2 * p.getY() * u.getY() - 2 * pz * uz) - 
+            4 * (-pow(p.getX()) - pow(p.getY()) - pow(pz)) * (pow(vn) - pow(vs) * pow(u.getX()) - pow(vs) * pow(u.getY()) - pow(vs) * pow(uz))
+        );
+
+        var t1 = 
+            ((-1) * vs * (-2 * p.getX() * u.getX() - 2 * p.getY() * u.getY() - 2 * pz * uz) - sqrtPart) / 
+            (2 * (pow(vn) - pow(vs)*pow(u.getX()) - pow(vs)*pow(u.getY()) - pow(vs)*pow(uz)));
+
+        var t2 = 
+            ((-1) * vs * (-2 * p.getX() * u.getX() - 2 * p.getY() * u.getY() - 2 * pz * uz) + sqrtPart) / 
+            (2 * (pow(vn) - pow(vs)*pow(u.getX()) - pow(vs)*pow(u.getY()) - pow(vs)*pow(uz)));
+
+        double t = 0.0;
+        if (!check(t1) && !check(t2)) {
+            return Optional.empty();
+        }else if (!check(t1) || !check(t2)){
+            t = check(t1) ? t1 : t2;
+        }else{
+            t = Math.min(t1, t2);
+        }
+
+        Translation2d outXY = p.plus(TempVelocity.times(t));
+        Translation3d out = new Translation3d(outXY.getX(), outXY.getY(), targetZ);
+
+        if (out.getNorm() > kMaxDistance) {
+            return Optional.empty();
+        }
+
+        //TODO make sure you understand this
+        return Optional.of(out); //relative to robot
+    }
 
     public static Optional<Translation2d> movingAutoAim(
         Translation2d speakPos, //S0
@@ -24,7 +84,6 @@ public class AutoAimFunctions {
 
         //FIXME account with vertical motion 
 
-        double kShooterSpeed = AutoAimConstants.kShooterSpeed; //s1
 
         double a = pow(speakVel.getX()) + pow(speakVel.getY()) - pow(kShooterSpeed);
         double b = 2 * ((speakPos.getX() * speakVel.getX()) + (speakPos.getY() * speakVel.getY()) /*- (P1.getX() * speakVel.getX()) - (P1.getY() * speakVel.getY())*/);
@@ -51,11 +110,11 @@ public class AutoAimFunctions {
     }
 
     public static boolean isSafeShot(Translation2d target){
-        return (target.getNorm() > AutoAimConstants.kMaxDistance);
+        return (target.getNorm() > kMaxDistance);
     }
 
     public static Rotation3d getShooterAngleUnsafe(Translation3d targetPos){ //this is the position of the speaker centered around the robot
-        double kRelHeight = targetPos.getZ() - AutoAimConstants.kShooterHeight;
+        double kRelHeight = targetPos.getZ() - kShooterHeight;
         double dist = targetPos.getNorm();
 
         return new Rotation3d(0, 
