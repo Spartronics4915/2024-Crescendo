c0919897796efe099b02328d70f29c55b261c9b8
Finn <148401810+FinnSpartronics@users.noreply.github.com>
2024-02-07 16:33:12 -0800

    Fixed limit switch so that it sets target

--- src/main/java/com/spartronics4915/frc2024/subsystems/Elevator.java
+++ src/main/java/com/spartronics4915/frc2024/subsystems/Elevator.java
@@ -100,9 +100,8 @@ public class Elevator extends SubsystemBase implements TrapezoidSimulatorInterfa
             manualControlUpdate();
         }
         // Not-manual
-        if (limitSwitch.get())
-            mTarget = Rotation2d.fromRotations(kLimitSwitchGoto);
-
+        if (limitSwitch.get()) mTarget = Rotation2d.fromRotations(kLimitSwitchGoto);
+        
         mCurrentState = mmmmmmmmmmmTrapezoid.calculate(
                 GeneralConstants.kUpdateTime,
                 mCurrentState,
