4b3c89aa87fa7b3aec97dd999c097e6507fc3059
otter502 <115671877+otter502@users.noreply.github.com>
2024-02-08 15:00:08 -0800

    added warmup function

--- src/main/java/com/spartronics4915/frc2024/commands/MovingAutoAimCommand.java
+++ src/main/java/com/spartronics4915/frc2024/commands/MovingAutoAimCommand.java
@@ -4,8 +4,10 @@ import com.spartronics4915.frc2024.subsystems.Shooter;
 import com.spartronics4915.frc2024.subsystems.ShooterWrist;
 import com.spartronics4915.frc2024.subsystems.swerve.SwerveDrive;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import static com.spartronics4915.frc2024.util.AutoAimFunctions.*;
 
@@ -14,7 +16,14 @@ public class MovingAutoAimCommand extends Command{
     public ShooterWrist mShooterWrist;
     public final Translation3d kTarget;
 
-    
+    static{
+        //CHECKUP "warms up" the function, first run of this function takes roughly 20 ms
+        movingAutoAim(
+            new Pose2d(0, 0, Rotation2d.fromDegrees(0)), 
+            new ChassisSpeeds(0, 0, 0), 
+            new Translation3d(0, 0, 0)
+        );
+    }
 
     public MovingAutoAimCommand(Translation3d mTarget) {
         this.mSwerve = SwerveDrive.getInstance();
