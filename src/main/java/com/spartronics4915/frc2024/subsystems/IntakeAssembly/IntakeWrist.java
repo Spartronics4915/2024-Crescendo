718212c026b7426586a242c3d18fcf3ada762f1c
otter502 <115671877+otter502@users.noreply.github.com>
2024-02-07 19:13:50 -0800

    format

--- src/main/java/com/spartronics4915/frc2024/subsystems/IntakeAssembly/IntakeWrist.java
+++ src/main/java/com/spartronics4915/frc2024/subsystems/IntakeAssembly/IntakeWrist.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
