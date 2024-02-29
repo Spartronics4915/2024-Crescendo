b6e3e9e5320cd1cf7cae9dd309678d68715310f0
sharkey300 <sharkey300@icloud.com>
2024-02-28 19:32:17 -0800

    bind aligntospeaker to x on driver controller

--- src/main/java/com/spartronics4915/frc2024/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2024/RobotContainer.java
@@ -17,6 +17,7 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.FollowPathHolonomic;
+import com.spartronics4915.frc2024.commands.AlignToSpeakerCommand;
 import com.spartronics4915.frc2024.commands.AutoComponents;
 import com.spartronics4915.frc2024.commands.BootCoralCommand;
 import com.spartronics4915.frc2024.commands.HomingCommand;
@@ -199,6 +200,7 @@ public class RobotContainer {
 
         mDriverController.leftTrigger(kDriverTriggerDeadband)
             .whileTrue(new LockOnCommand());
+        mDriverController.x().onTrue(new AlignToSpeakerCommand());
 
         // Operator controls
         // Buttons:
