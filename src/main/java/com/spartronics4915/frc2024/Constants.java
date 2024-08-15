d2f68dd11a82bd7cd2f845c62fd601e3e420001e
otter502 <115671877+otter502@users.noreply.github.com>
2024-08-14 17:26:35 -0700

    set pid controller to tuned ish values, switched to encoder based rotation

--- src/main/java/com/spartronics4915/frc2024/Constants.java
+++ src/main/java/com/spartronics4915/frc2024/Constants.java
@@ -335,7 +335,7 @@ public final class Constants {
             final double maxMotorPowerSetting = 1;
             final double P = maxMotorPowerSetting / motorRotationsNeedingFullPower;
 
-            kPIDconstants = new PIDConstants(6, 0/*0.25*/, 0.02);
+            kPIDconstants = new PIDConstants(15, 0.01, 0.1);
         }
 
         public static final Constraints kConstraints;
